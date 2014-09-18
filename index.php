09bc30867c9a15a316321881266ba87bc3922109
Nik Yaroshevsky <nyaroshevsky@gmail.com>
2014-09-17 23:20:38 -0400

    renamed index, took out runatserver stuff

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <style>
+	
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			min-width:600px;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:50%;
+		}
+	
+	</style>
+    <title>Nik Yaroshevsky Portfolio Page</title>
+</head>
+<body>
+    <form id="form1">
+        <div class="container">
+	
+		<h1>Doris Flores <br> Dynamic Web Apps Portfolio</h1>
+		
+		<img src='http://p1.nyaroshevsky.me/images/avatar.jpg' alt='Doris Flores'>
+		
+		<h3>Project 2 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 3 Title</h3>
+		View: [link]
+		Github: [link]
+		
+		<h3>Project 4 Title</h3>
+		View: [link]
+		Github: [link]
+
+
+		
+		
+		
+		
+		<h2>What I do/</h2>
+		<p>I am a software engineer, my experience is with the full stack of development from high level client side, server web development to back end c++ and java. </p>
+		
+		<h2>Why am I taking this course?</h2>
+		<p>I would like to complete the WAD prerequisite for my master's degree.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>It is safe to say that I am quite familiar with it, but I never stop learning.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I work on all the platforms, primarily on Mac OsX, windows, linux.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>I was born in Russia after the collapse of the soviet union.</p>
+	
+	</div>
+    </form>
+</body>
+</html>
+
+
