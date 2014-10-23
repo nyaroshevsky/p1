33695152273db1bc9180a73f85f0d9e298eb2db9
Nik Yaroshevsky <nyaroshevsky@gmail.com>
2014-10-23 15:23:46 -0400

    Corrected links

--- index.php
+++ index.php
@@ -35,11 +35,11 @@
                 <img src='http://p1.nyaroshevsky.me/images/avatar.jpg'  alt='Nik Yaroshevsky'>
 
                 <h3>Project 2 Title</h3>
-                 <a href="p2.nyaroshevsky.me">View: [link]</a>
+                 <a href="http://p2.nyaroshevsky.me">View: [link]</a>
         <a href="https://github.com/nyaroshevsky/p2">Github: [link] </a> 
         
         <h3>Project 3 Title</h3>
-                <a href="p3.nyaroshevsky.me">View: [link]</a>
+                <a href="http://p3.nyaroshevsky.me">View: [link]</a>
         <a href="https://github.com/nyaroshevsky/p3">Github: [link] </a> 
         
         <h3>Project 4 Title</h3>
